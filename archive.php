486147e304ea9e9993d52f81317889d6c72500f2
Marcelo Somers <msomers@gmail.com>
2014-01-05 08:11:01 -0600

    Added careers to the archive page

--- archive.php
+++ archive.php
@@ -25,7 +25,7 @@ get_header(); ?>
         </div>
 
         <div class="archive-category-box">
-            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/design.svg" alt="Business"></div>
+            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/design.svg" alt="Design"></div>
             <div class="archive-category-description">
                 <h3><a href="<?php echo home_url() ?>/category/design/">Design</a></h3>
                 <p>Articles and links spanning the gamut of design, user experience, and building delightful products.</p>
@@ -33,7 +33,7 @@ get_header(); ?>
         </div>
 
         <div class="archive-category-box">
-            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/code.svg" alt="Business"></div>
+            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/code.svg" alt="Code"></div>
             <div class="archive-category-description">
                 <h3><a href="<?php echo home_url() ?>/category/code/">Code</a></h3>
                 <p>JavaScript, Ruby on Rails, PHP, and anything in between.</p>
@@ -41,7 +41,15 @@ get_header(); ?>
         </div>
 
         <div class="archive-category-box">
-            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/guides.svg" alt="Business"></div>
+            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/career.svg" alt="Career"></div>
+            <div class="archive-category-description">
+                <h3><a href="<?php echo home_url() ?>/category/career/">Career</a></h3>
+                <p>Is it self-help? Maybe, but this is everything related to improving yourself as a professional.</p>
+            </div>
+        </div>
+
+        <div class="archive-category-box">
+            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/guides.svg" alt="Guides"></div>
             <div class="archive-category-description">
                 <h3><a href="<?php echo home_url() ?>/category/guides/">Guides</a></h3>
                 <p>How to do cool stuff.</p>
@@ -49,7 +57,7 @@ get_header(); ?>
         </div>
 
         <div class="archive-category-box">
-            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/announcement.svg" alt="Business"></div>
+            <div class="archive-icon"><img src="<?php bloginfo('template_directory'); ?>/images/announcement.svg" alt="Site Announcements"></div>
             <div class="archive-category-description">
                 <h3><a href="<?php echo home_url() ?>/category/announcement/">Announcements</a></h3>
                 <p>Anything related to this site itself or personal news.</p>
