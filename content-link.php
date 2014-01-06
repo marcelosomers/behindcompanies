d813b17de8c016745ea278b2ab8cbf2cd3b5afd9
Marcelo Somers <msomers@gmail.com>
2014-01-05 20:53:16 -0600

    Added links to the category on each post

--- content-link.php
+++ content-link.php
@@ -10,7 +10,7 @@
     <div class="category">
         <?php
             $category = get_the_category();
-            echo $category[0]->cat_name;
+            echo "<a href='" . home_url() . "/category/" . $category[0]->cat_name . "'>" . $category[0]->cat_name . "</a>";
         ?>
     </div>
     <h2 class="title title--link"><a href="<?php the_linked_list_link() ?>"><?php the_title(); ?></a> <span class="link-permalink"><a href="<?php the_permalink() ?>">&#9642;</a></span></h2>
--- content.php
+++ content.php
@@ -10,7 +10,7 @@
     <div class="category">
         <?php
             $category = get_the_category();
-            echo $category[0]->cat_name;
+            echo "<a href='" . home_url() . "/category/" . $category[0]->cat_name . "'>" . $category[0]->cat_name . "</a>";
         ?>
     </div>
     <h2 class="title title--article"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
