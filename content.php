92dae8eeb066d56decfa8b81c0b571b6a4e912bb
Marcelo Somers <msomers@gmail.com>
2014-01-06 22:12:56 -0600

    Updated article typography and ol styling

--- content.php
+++ content.php
@@ -13,6 +13,6 @@
             echo "<a href='" . home_url() . "/category/" . $category[0]->cat_name . "'>" . $category[0]->cat_name . "</a>";
         ?>
     </div>
-    <h2 class="title title--article"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+    <h2 class="title title--article"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php the_content(); ?>
 </article>
